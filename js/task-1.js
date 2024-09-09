d72779a07fb09442d8d354d81aee3ebb502f8f73
Anna Fleck <annafleck18@gmail.com>
2024-09-09 17:04:58 +0300

    add

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const itemEl = document.querySelectorAll("li.item");
-console.log("Number of categories:", itemEl.length);
-
-itemEl.forEach((item) => {
-  const titleEl = item.firstElementChild;
-  console.log("Category:", titleEl.textContent);
-  const listEl = item.lastElementChild;
-  console.log("Elements:", listEl.children.length);
-});
